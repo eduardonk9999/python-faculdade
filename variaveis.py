19d7fe74dce0b2c6984eb6c74049c0c1a32238e0
Eduardo Silva <thiago.novak7@gmail.com>
2025-03-28 10:12:24 -0300

    aulas

---
+++ variaveis.py
@@ -0,0 +1,8 @@
+# type() para exibir o tipo de variavel
+
+ano = 1989
+nome = "Luke Skywalker"
+saldo = 50.30
+print(("o tipo da variável ano é {}".format(type(ano))))
+print(("o tipo da variável nome é {}".format(type(nome))))
+print(("o tipo da variável saldo é {}".format(type(saldo))))

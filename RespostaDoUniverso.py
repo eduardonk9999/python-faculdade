d21795ba9e26afe169b3b3a43f7e494607f3b5ef
Eduardo Silva <thiago.novak7@gmail.com>
2025-04-13 09:08:16 -0300

    exemplos

---
+++ RespostaDoUniverso.py
@@ -0,0 +1,6 @@
+resposta = "0"
+
+while (resposta != "42"):
+
+    resposta = input("Qual a resposta para a vida, o univer e tudo mais? ")
+print("Parabéns, você acertou!")    
---
+++ for.py
@@ -0,0 +1,8 @@
+for x in range(10):
+    print(x)
+
+# repetições em intervalos
+for dia in ["segunda","terça","quarta","quinta","sexta"]:
+    if dia == "segunda" or dia == "quarta" or dia == "sexta":
+        print("É dia de colocar o lixo para o coletor.")
+
